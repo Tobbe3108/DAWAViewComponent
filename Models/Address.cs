4253cf1a179dd4edb2fb76275e0f1b1bf5a5ff46
Tobias <Tobbe3108@gmail.com>
2019-09-26 13:27:14 +0200

    Initial commit

---
+++ Models/Address.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAWAViewComponent.Models
+{
+    public class Address
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ ViewComponents/SimpleViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAWAViewComponent.ViewComponents
+{
+    [ViewComponent(Name = "DAWAViewComponent.Simple")]
+    public class SimpleViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //var address = new Models.Address();
+            return View();
+        }
+    }
+}
